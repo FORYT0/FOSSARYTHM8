9c0d1b4ee1f3d90c4563d57aaffe20f7df14be53
Pete-rise <kung.u.pete@gmail.com>
2026-03-15 09:16:58 +0300

    Optimize bundling: split recharts and vendor chunks

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('recharts') || id.includes('d3')) {
+            return 'charts'
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
   },
 })
